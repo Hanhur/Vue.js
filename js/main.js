5df779fc58dc1fe0fcbe83786286714016b592f7
Vladimír Hanhur <aqgz@email.cz>
2024-03-30 21:22:44 +0100

    update

---
+++ js/main.js
@@ -0,0 +1,26 @@
+
+const Counter = {
+    data() 
+    {
+        return {
+            // counter: 0
+        }
+    },
+    mounted() 
+    {
+        setInterval(() => {
+            this.counter++;
+        }, 1000);
+    },
+}
+Vue.createApp(Counter).mount('#counter');
+
+const AttributeBinding = {
+    data() 
+    {
+        return {
+            message: 'Страница загружена ' + new Date().toLocaleString()
+        }
+    }
+}
+Vue.createApp(AttributeBinding).mount('#bind-attribute');
